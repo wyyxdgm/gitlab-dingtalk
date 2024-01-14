0a7b19811f423cf532b4644f239c84a455dd2d60
Damo <wyyxdgm@163.com>
2024-01-14 23:57:39 +0800

    test

--- test/event.js
+++ test/event.js
@@ -1,11 +1,12 @@
-const event = "Merge Request Hook";
-// "Issue Hook";
+// const event = "Merge Request Hook";
+// const event = "Issue Hook";
+const event = "Tag Push Hook";
 const { HEADER_EVENTS_TO_KEY, PROP_TO_KEY } = require("../src/v.js");
 console.log(event);
 let fName = HEADER_EVENTS_TO_KEY[event];
 const templateName = process.env.TEMPLATE_NAME || "default";
 const template = require(`../src/templates/${templateName}`);
-const data = require("./events.js").mr;
+const data = require("./events.js").tag;
 if (!fName) {
   for (let k in PROP_TO_KEY) {
     if (data[k]) {
@@ -17,5 +18,6 @@ if (!fName) {
 if (!fName) {
   return res.status(400).send("Bad Request");
 }
+
 let msgOption = template[fName]?.(data) || template._?.(data);
 console.log(msgOption);
